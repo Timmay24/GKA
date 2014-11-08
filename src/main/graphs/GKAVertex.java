14ab83cf197a09c4fd4a94366be9e266b6a669e6
Timmay24 <tim.hartig@haw-hamburg.de>
2014-11-08 19:04:50 +0100

    GKAVertex implementiert nun compareTo aus Comparable. Artenname aus Getter und Setter entfernt (nodeWeight --> weight). Parent ist nun eine Referenz auf einen anderen Knoten, statt seinem Namen als String. weight Typ geändert zu Integer. Doc Tags nachgerüstet.

--- src/main/graphs/GKAVertex.java
+++ src/main/graphs/GKAVertex.java
@@ -1,11 +1,11 @@
 package main.graphs;
 
-public class GKAVertex {
+public class GKAVertex implements Comparable<GKAVertex> {
 
 	private		String 		name;
-	private 	String 		parent;
-	private 	int 		nodeWeight;
-	private 	boolean 	visited = false;
+	private 	GKAVertex	parent;
+	private 	Integer 	weight;
+	private 	boolean 	visited;
 
 	/**
 	 * @return Gibt den Knotennamen zurueck.
@@ -17,27 +17,33 @@ public class GKAVertex {
 	/**
 	 * Gibt die Knotengewichtung zurueck (benoetigt fuer BFS)
 	 * 
-	 * @param nodeWeight
+	 * @param weight
 	 */
-	public int getNodeWeight() {
-		return nodeWeight;
+	public Integer getWeight() {
+		return weight;
 	}
 
 	/**
 	 * Setzt die Knotengewichtung (benoetigt fuer BFS)
 	 * 
-	 * @param nodeWeight Knotengewicht
+	 * @param weight Knotengewicht
 	 */
-	public void setNodeWeight(int nodeWeight) {
-		this.nodeWeight = nodeWeight;
+	public void setWeight(Integer weight) {
+		this.weight = weight;
 	}
 
 
-	public void setParent(String parent){
+	/**
+	 * @param Referenz des Vorgaengers dieses Knotens
+	 */
+	public void setParent(GKAVertex parent) {
 		this.parent = parent;
 	}
 	
-	public String getParent(){
+	/**
+	 * @return Referenz des Vorgaengers dieses Knotens
+	 */
+	public GKAVertex getParent() {
 		return this.parent;
 	}
 
@@ -57,27 +63,45 @@ public class GKAVertex {
 	 * Konstruktor
 	 * 
 	 * @param name Name des Knotens.
+	 * @param weight Gewicht des Knotens.
 	 */
-	private GKAVertex(String name) {
+	private GKAVertex(String name, Integer weight) {
 		this.name = name;
-//		this.color = "white";
+		this.visited = false;
+		this.weight = weight;
 	}
 
 	/**
 	 * Factory-Methode zur Erzeugung einer Instanz.
 	 * 
 	 * @param name Name des Knotens
+	 * @param weight Gewicht des Knotens
 	 * @return Gibt eine neue Instanz der Klasse zurueck.
 	 */
-	public static GKAVertex valueOf(String name) {
-		return new GKAVertex(name);
+	public static GKAVertex valueOf(String name, Integer weight) {
+		return new GKAVertex(name, weight);
 	}
+	
+	/**
+	 * Konfiguration: festes Gewicht
+	 */
+	public static GKAVertex valueOf(String name) {
+		return valueOf(name, 0);
+	}
+	
+	
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return getName();
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -86,6 +110,9 @@ public class GKAVertex {
 		return result;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -101,6 +128,14 @@ public class GKAVertex {
 		} else if (!name.equals(other.name)) //TODO pruefen, ob Abgleich mit dem Namen korrekt ist
 			return false;
 		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
+	@Override
+	public int compareTo(GKAVertex other) {
+		return this.getWeight().compareTo(other.getWeight());
 	}
 
 }
