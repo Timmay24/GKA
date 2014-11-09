c59590f5e78c140f87187dc74b792f495ebe7a17
Timmay24 <tim.hartig@haw-hamburg.de>
2014-11-09 09:40:56 +0100

    Doctags und Kommentare hinzugefuegt.

--- src/main/graphs/Matrix.java
+++ src/main/graphs/Matrix.java
@@ -4,14 +4,29 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * @author Timmay
+ *
+ * @param <RK> Typ des Zeilenschluessels
+ * @param <CK> Typ des Spaltenschluessels
+ * @param <V>  Typ des Zellenwertes 
+ */
 public class Matrix <RK,CK,V> {
 
 	Map<RK, Map<CK,V> > matrix;
 	
+	/**
+	 * KONSTRUKTOR
+	 */
 	public Matrix() {
 		matrix = new HashMap<>();
 	}
 	
+	/**
+	 * @param row Zeilenschluessel
+	 * @param column Spaltenschluessel
+	 * @return Wert, der hinter der Kombination (row,column) hinterlegt ist.
+	 */
 	public V getValueAt(RK row, CK column) {
 		if (!matrix.containsKey(row)) {
 			return null;
@@ -24,14 +39,25 @@ public class Matrix <RK,CK,V> {
 		return matrix.get(row).get(column);
 	}
 	
+	/**
+	 * @param row Zeilenschluessel
+	 * @param column Spaltenschluessel
+	 * @param value Einzutragener Wert
+	 */
 	public void setValueAt(RK row, CK column, V value) {
 		Map<CK,V> columnMap = matrix.get(row);
 		
+		// Falls zu row kein Wert existiert, 
 		if (columnMap == null) {
+			// wird eine neue Werte-Map (columnMap) erzeugt
 			columnMap = new HashMap<>();
+			// und in ihr zum Schluessel column, value eingetragen.
 			columnMap.put(column, value);
+			// Zum Schluss wird die columnMap bei row eingetragen.
 			matrix.put(row, columnMap);
+		// Falls bereits eine Wertemap (columnMap) hinter row existiert,
 		} else {
+			// wird value in ihr zu column eingetragen.
 			columnMap.put(column, value);
 		}
 	}
