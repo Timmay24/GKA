117cbe734dd0c7c5ed8e9631dff2587ff1e12d82
Timmay24 <tim.hartig@haw-hamburg.de>
2014-11-12 23:42:26 +0100

    MainWindow: Deklarationen sortiert.

--- src/gui/GraphPopUp.java
+++ src/gui/GraphPopUp.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
